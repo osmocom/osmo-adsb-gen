efc57bf72df2b33fbaaccd58e65ac3a63a62e533
Harald Welte <laforge@gnumonks.org>
2015-04-01 17:56:11 +0200

    change include path from gruel -> gnuradio

--- gr-adsbtx/include/adsbtx/api.h
+++ gr-adsbtx/include/adsbtx/api.h
@@ -22,7 +22,7 @@
 #ifndef INCLUDED_ADSBTX_API_H
 #define INCLUDED_ADSBTX_API_H
 
-#include <gruel/attributes.h>
+#include <gnuradio/attributes.h>
 
 #ifdef gnuradio_ADSBTX_EXPORTS
 #  define ADSBTX_API __GR_ATTR_EXPORT
